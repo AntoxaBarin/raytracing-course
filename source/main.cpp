d0ab670479149b3cd53915356c22ace9d8d195f1
Ivan Shanygin <shanygin101@yandex.ru>
2025-03-15 18:54:07 +0300

    [Refactoring]: add verbosity flag, update scene operator<<

--- source/main.cpp
+++ source/main.cpp
@@ -1,18 +1,32 @@
+#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 
 #include "io.hpp"
 
+static bool verbose = false;
+
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cout << "Usage: ./engine <path-to-scene> <path-to-image>\n";
-        return 1;
+    if (argc != 3 && argc != 4) {
+        std::cout << "Usage: ./engine <path-to-scene> <path-to-image> [-v]\n";
+        return EXIT_FAILURE;
+    }
+    if (argc == 4 && std::string(argv[3]) != "-v") {
+        std::cout << "Unknown argument: " << argv[3] << '\n';
+        return EXIT_FAILURE;
+    }
+    else if (argc == 4 && std::string(argv[3]) == "-v") {
+        verbose = true;
     }
 
     try {
         engine::Scene scene = engine::io::load_scene(std::string(argv[1]));
         engine::Image image = engine::generate_image(scene);
         engine::io::write_image(std::string(argv[2]), scene.width, scene.height, image);
+    
+        if (verbose) {
+            std::cout << scene << '\n';
+        }
     }
     catch (const std::runtime_error& e) {
         std::cerr << e.what() << std::endl;
--- source/scene.cpp
+++ source/scene.cpp
@@ -14,8 +14,10 @@ Scene::~Scene() {
 
 std::ostream& operator<<(std::ostream& a_out, const Scene& a_scene) {
     a_out << "H: " << a_scene.height << " W: " << a_scene.width << '\n'
-          << "BG_COLOR: " << a_scene.bg_color.x << ' ' << a_scene.bg_color.y << ' ' << a_scene.bg_color.z << '\n'
-          << "CAMERA.\nFOV_X: " << a_scene.camera.camera_fov_x << '\n'
+          << "BG_COLOR (R G B): " << a_scene.bg_color.r << ' ' << a_scene.bg_color.g << ' ' << a_scene.bg_color.b << '\n'
+          << "AMBIENT_LIGHT (R G B): " << a_scene.ambient_light.r << ' ' << a_scene.ambient_light.g << ' ' << a_scene.ambient_light.b << "\n\n"
+          << "RAY_DEPTH: " << a_scene.ray_depth << '\n'
+          << "CAMERA:\nFOV_X: " << a_scene.camera.camera_fov_x << '\n'
           << "POSITION: " << a_scene.camera.camera_position.x << ' ' << a_scene.camera.camera_position.y << ' '
           << a_scene.camera.camera_position.z << '\n'
           << "RIGHT: " << a_scene.camera.camera_right.x << ' ' << a_scene.camera.camera_right.y << ' ' << a_scene.camera.camera_right.z
@@ -25,19 +27,19 @@ std::ostream& operator<<(std::ostream& a_out, const Scene& a_scene) {
           << a_scene.camera.camera_forward.z << "\n\n";
 
     for (auto primitive : a_scene.primitives) {
-        a_out << "Primitive: ";
+        a_out << "Primitive:\n";
         if (primitive->type == PRIMITIVE_TYPE::Plane) {
-            a_out << "Plane\n";
+            a_out << "Plane ";
             Plane* plane = dynamic_cast<Plane*>(primitive);
             a_out << plane->normal.x << ' ' << plane->normal.y << ' ' << plane->normal.z << '\n';
         }
         else if (primitive->type == PRIMITIVE_TYPE::Ellipsoid) {
-            a_out << "Ellipsoid\n";
+            a_out << "Ellipsoid ";
             Ellipsoid* ellips = dynamic_cast<Ellipsoid*>(primitive);
             a_out << ellips->radius.x << ' ' << ellips->radius.y << ' ' << ellips->radius.z << '\n';
         }
         else if (primitive->type == PRIMITIVE_TYPE::Box) {
-            a_out << "Box\n";
+            a_out << "Box ";
             Box* plane = dynamic_cast<Box*>(primitive);
             a_out << plane->size.x << ' ' << plane->size.y << ' ' << plane->size.z << '\n';
         }
@@ -47,10 +49,10 @@ std::ostream& operator<<(std::ostream& a_out, const Scene& a_scene) {
 
     for (auto light : a_scene.lights) {
         a_out << "Light:\n"
-              << light->intensity.r << ' ' << light->intensity.g << ' ' << light->intensity.b << '\n'
-              << light->direction.x << ' ' << light->direction.y << ' ' << light->direction.z << '\n'
-              << light->position.x << ' ' << light->position.y << ' ' << light->position.z << '\n'
-              << light->attenuation.x << ' ' << light->attenuation.y << ' ' << light->attenuation.z << "\n\n";
+              << "Intensity: " << light->intensity.r << ' ' << light->intensity.g << ' ' << light->intensity.b << '\n'
+              << "Direction: " << light->direction.x << ' ' << light->direction.y << ' ' << light->direction.z << '\n'
+              << "Position: " << light->position.x << ' ' << light->position.y << ' ' << light->position.z << '\n'
+              << "Attenuation: " << light->attenuation.x << ' ' << light->attenuation.y << ' ' << light->attenuation.z << "\n\n";
     }
 
     return a_out;
