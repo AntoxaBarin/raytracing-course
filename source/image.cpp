7fa3c0cbcde274021bc80a856118a2f59de657a8
Ivan Shanygin <shanygin101@yandex.ru>
2025-04-01 23:39:33 +0300

    [HW 3]: Add multi-threaded version of engine::generate_image

--- source/image.cpp
+++ source/image.cpp
@@ -2,9 +2,16 @@
 #include "ray.hpp"
 #include "utils.hpp"
 
+#include <thread>
+
 namespace engine {
 
-Image generate_image(const Scene& scene) {
+Image generate_image_multithread(const Scene& scene);
+
+Image generate_image(const Scene& scene, bool multithread) {
+    if (multithread) {
+        return generate_image_multithread(scene);
+    }
     Image result{};
     result.resize(scene.height * scene.width * 3);
 
@@ -26,6 +33,55 @@ Image generate_image(const Scene& scene) {
             result[index + 2] = color_converter(mean_color.b);
         }
     }
+    return result;
+}
+
+Image generate_image_multithread(const Scene& scene) {
+    Image result{};
+    result.resize(scene.height * scene.width * 3);
+
+    const std::size_t total_pixels = scene.height * scene.width;
+    const std::size_t threads_num = std::max(4u, std::thread::hardware_concurrency());
+    std::vector<std::thread> threads;
+    threads.reserve(threads_num);
+
+    const std::uint32_t W = scene.width;
+    const std::uint32_t H = scene.height;
+
+    const std::size_t base_chunk_size = total_pixels / threads_num;
+    std::size_t offset = total_pixels % threads_num;
+
+    auto worker = [&scene, W, &result](std::size_t begin, std::size_t end) {
+        for (size_t i = begin; i < end; ++i) {
+            const size_t row = i / W;
+            const size_t col = i % W;
+
+            glm::vec3 mean_color{};
+            for (int k = 0; k < scene.samples; ++k) {
+                ray::Ray ray = ray::generate_ray(scene, {col, row});
+                const auto& [_, rawcolor] = ray::raytrace(ray, scene, 0);
+                mean_color += rawcolor;
+            }
+            mean_color /= static_cast<float>(scene.samples);
+
+            const size_t index = i * 3;
+            result[index] = color_converter(mean_color.r);
+            result[index + 1] = color_converter(mean_color.g);
+            result[index + 2] = color_converter(mean_color.b);
+        }
+    };
+
+    std::size_t start = 0;
+    for (std::size_t i = 0; i < threads_num; ++i) {
+        const std::size_t chunk_size = base_chunk_size + (i < offset ? 1 : 0);
+        threads.emplace_back(worker, start, start + chunk_size);
+        start += chunk_size;
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
     return result;
 }
 
--- source/main.cpp
+++ source/main.cpp
@@ -5,28 +5,35 @@
 #include "io.hpp"
 
 static bool verbose = false;
+static bool multithread = false;
 
 int main(int argc, char* argv[]) {
-    if (argc != 3 && argc != 4) {
-        std::cout << "Usage: ./engine <path-to-scene> <path-to-image> [-v]\n";
+    if (argc < 3 || argc > 5) {
+        std::cout << "Usage: ./engine <path-to-scene> <path-to-image> [-v] [-thread]\n";
         return EXIT_FAILURE;
     }
-    if (argc == 4 && std::string(argv[3]) != "-v") {
-        std::cout << "Unknown argument: " << argv[3] << '\n';
-        return EXIT_FAILURE;
-    }
-    else if (argc == 4 && std::string(argv[3]) == "-v") {
-        verbose = true;
+    if (argc > 3) {
+        for (std::size_t i = 3; i < argc; ++i) {
+            if (std::string(argv[i]) == "-v") {
+                verbose = true;
+            }
+            else if (std::string(argv[i]) == "-thread") {
+                multithread = true;
+            }
+            else {
+                std::cout << "Unknown argument: " << argv[i] << '\n';
+                return EXIT_FAILURE;
+            }
+        }
     }
 
     try {
         engine::Scene scene = engine::io::load_scene(std::string(argv[1]));
-        engine::Image image = engine::generate_image(scene);
-        engine::io::write_image(std::string(argv[2]), scene.width, scene.height, image);
-
         if (verbose) {
             std::cout << scene << '\n';
         }
+        engine::Image image = engine::generate_image(scene, multithread);
+        engine::io::write_image(std::string(argv[2]), scene.width, scene.height, image);
     }
     catch (const std::runtime_error& e) {
         std::cerr << e.what() << std::endl;
